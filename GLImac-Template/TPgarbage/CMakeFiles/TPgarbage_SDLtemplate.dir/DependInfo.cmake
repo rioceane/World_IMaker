2cedecccfdceb2ea7a2f3ce51029e2229554114d
oceane <rioce.oceane@gmail.com>
2019-12-06 12:25:05 +0100

    Première structure
    Reprise des TP d'opengl

---
+++ GLImac-Template/TPgarbage/CMakeFiles/TPgarbage_SDLtemplate.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/6im2/sveysset/Documents/OpenGL 3D/GLImac-Template/TPgarbage/SDLtemplate.cpp" "/home/6im2/sveysset/Documents/OpenGL 3D/GLImac-Template/TPgarbage/CMakeFiles/TPgarbage_SDLtemplate.dir/SDLtemplate.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/SDL"
+  "glimac/include"
+  "third-party/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/6im2/sveysset/Documents/OpenGL 3D/GLImac-Template/glimac/CMakeFiles/glimac.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ GLImac-Template/TPgarbage/CMakeFiles/TPgarbage_foo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TPgarbage_foo.dir/foo.cpp.o"
+  "TPgarbage_foo.pdb"
+  "TPgarbage_foo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TPgarbage_foo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
