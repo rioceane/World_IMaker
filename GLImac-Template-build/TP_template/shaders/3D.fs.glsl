48e00abc9729b130c21851221ad943f02a16bbb5
oceane <rioce.oceane@gmail.com>
2020-01-09 23:40:05 +0100

    Finalisation du code

--- GLImac-Template-build/TP_template/shaders/3D.fs.glsl
+++ GLImac-Template-build/TP_template/shaders/3D.fs.glsl
@@ -3,7 +3,7 @@
 out vec4 fragColor;
 
 in vec3 vNormale;
-in vec3 vPositionmonde;
+in vec3 vPosition;
 
 uniform vec3 uLumieredirection;
 uniform vec3 uLumierepoint;
@@ -11,7 +11,7 @@ uniform vec3 uCouleur;
 
 void main() {
 	float luminosite = max(-dot(uLumieredirection,vNormale),0.2);
-	vec3 direction=normalize(vPositionmonde-uLumierepoint);
+	vec3 direction=normalize(vPosition-uLumierepoint);
 	float luminositeponctuelle=max(-dot(direction,vNormale),0.2);
 	float additionluminosite = min(luminosite+luminositeponctuelle, 1);
 	vec3 color = uCouleur;
